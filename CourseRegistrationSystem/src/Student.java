4bea1a50b346455a0f5e2fb79116a1c6784920c7
Necati Koçak <necatikocak49@gmail.com>
2023-11-16 19:47:26 +0300

    Update Student.java
    clearRequestedCourses() method updated

--- CourseRegistrationSystem/src/Student.java
+++ CourseRegistrationSystem/src/Student.java
@@ -1,5 +1,10 @@
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,8 +40,36 @@ class Student extends User {
     }
 
     public void clearRequestedCourses() {
+
+        try {
+            // JSON file will be read
+            File jsonFile = new File("src/Students/150120001.json");
+
+            // Read JSON data as follows
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(jsonFile);
+
+            // Find the "requestedCourses" field
+            JsonNode requestedCoursesNode = rootNode.get("requestedCourses");
+
+            // If the "requestedCourses" field is not empty, delete the content
+            if (requestedCoursesNode != null && requestedCoursesNode.isArray()) {
+                ArrayNode requestedCoursesArray = (ArrayNode) requestedCoursesNode;
+                requestedCoursesArray.removeAll();
+                System.out.println("requestedCourses content has been deleted.");
+            } else {
+                System.out.println("The requestedCourses field is already empty or not found.");
+            }
+
+            // Write changes to JSON file
+            objectMapper.writeValue(jsonFile, rootNode);
+
+            System.out.println("The changes are saved.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         this.requestedCourses.clear();
-        // JSONDA REQUEST SİLİNECEK
     }
 
     public String getStudentId() {
